60acb84ce3d0c6c1de33b802e9b4526e5ac3f8c4
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-10-01 21:06:29 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    // Returns all drivers that match the passed in name (case-insensitive)
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    // Returns drivers whose names begin with the provided substring
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    // Returns all objects whose 'name' property matches the passed in name
+    return drivers.filter(driver => driver.name === name);
+  }
+  
